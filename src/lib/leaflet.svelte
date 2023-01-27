8700daf60fc5a81bc78d432496cedf6a8b3e1b47
Kandreas9 <andkarido@gmail.com>
2023-01-27 20:40:40 +0100

    fixed leaflet window bug

--- src/lib/leaflet.svelte
+++ src/lib/leaflet.svelte
@@ -1,49 +1,54 @@
 <script>
-    import { onMount, onDestroy } from 'svelte';
-    import { browser } from '$app/environment';
-    import leaflet from 'leaflet'
+	import { onMount, onDestroy } from 'svelte';
+	import { browser } from '$app/environment';
 
-    let mapElement;
-    let map;
+	let mapElement;
+	let map;
 
-    function centerLeafletMapOnMarker(map, marker) {
-        let latLngs = [ marker.getLatLng() ];
-        let markerBounds = leaflet.latLngBounds(latLngs);
-        map.fitBounds(markerBounds);
-    }
+	onMount(async () => {
+		if (browser) {
+			const leaflet = await import('leaflet');
 
-    onMount(async () => {
-        if(browser) {
-            map = leaflet.map(mapElement).setView([51.505, -0.09], 13);
+			function centerLeafletMapOnMarker(map, marker) {
+				let latLngs = [marker.getLatLng()];
+				let markerBounds = leaflet.latLngBounds(latLngs);
+				map.fitBounds(markerBounds);
+			}
 
-            leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            }).addTo(map);
+			map = leaflet.map(mapElement).setView([51.505, -0.09], 13);
 
-            let marker = leaflet.marker([43.56131071912586, 3.9513040418714134]).addTo(map)
-                .bindPopup('Centre Médical du Littoral')
-                .openPopup();
+			leaflet
+				.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+					attribution:
+						'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+				})
+				.addTo(map);
 
-            centerLeafletMapOnMarker(map, marker)
-        }
-    });
+			let marker = leaflet
+				.marker([43.56131071912586, 3.9513040418714134])
+				.addTo(map)
+				.bindPopup('Centre Médical du Littoral')
+				.openPopup();
 
-    onDestroy(async () => {
-        if(map) {
-            console.log('Unloading Leaflet map.');
-            map.remove();
-        }
-    });
+			centerLeafletMapOnMarker(map, marker);
+		}
+	});
+
+	onDestroy(async () => {
+		if (map) {
+			console.log('Unloading Leaflet map.');
+			map.remove();
+		}
+	});
 </script>
 
-
-<div bind:this={mapElement}></div>
+<div bind:this={mapElement} />
 
 <style>
-    @import 'leaflet/dist/leaflet.css';
-    div {
-        height: 300px;
-        width: 100vw;
-        margin: 2rem 0 0 0;
-    }
+	@import 'leaflet/dist/leaflet.css';
+	div {
+		height: 300px;
+		width: 100vw;
+		margin: 2rem 0 0 0;
+	}
 </style>
