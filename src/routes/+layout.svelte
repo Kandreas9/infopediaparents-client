f2355cf799b36227bc50e9a457ab28ea73ba02ea
Kandreas9 <andkarido@gmail.com>
2023-01-29 00:03:49 +0100

    added cookie consent

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,24 @@
 	import '../global.css';
 	import Nav from '$lib/nav.svelte';
 	import Footer from '$lib/footer.svelte';
+
+	import { GoogleAnalytics } from '@beyonk/svelte-google-analytics';
+	import '@beyonk/gdpr-cookie-consent-banner/dist/style.css'; // optional, you can also define your own styles
+	import GdprBanner from '@beyonk/gdpr-cookie-consent-banner';
+
+	let ga;
+
+	function initAnalytics() {
+		ga.init();
+	}
 </script>
+
+<GdprBanner
+	description="Welcome! Please consider letting us to have your consent to store cookies"
+	on:analytics={initAnalytics}
+/>
+
+<GoogleAnalytics bind:this={ga} properties={['G-JY254ZZ6R8']} enabled={false} />
 
 <Nav />
 
