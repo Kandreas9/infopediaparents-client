eb7610b152c9e43aa4b823bf6dcfe120b55cd068
Kandreas9 <andkarido@gmail.com>
2023-02-06 22:49:47 +0100

    css breaking fixes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -34,7 +34,7 @@
 	<a href="fiches?tag=proprete,sommeil" class="box"
 		>Enfant normal et ses variations limites de la normale</a
 	>
-	<a href="#" class="box disabled">Les signes D'ALERTE</a>
+	<a href="fiches?tag=Signes D'alerte" class="box">Les signes D'ALERTE</a>
 	<a href="#" class="box disabled">Les Bons Gestes</a>
 	<a href="#" class="box disabled">Mon enfant doit prendre UN MEDICAMENT</a>
 	<a href="fiches?tag=alimentation" class="box">Alimentation</a>
@@ -92,7 +92,7 @@
 		color: white;
 		background-color: blue;
 		padding: 1rem;
-		height: 4rem;
+		height: auto;
 		text-decoration: none;
 	}
 
