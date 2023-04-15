c4e4153d64dbd6478ab370f332a4eb868c34ab77
Kandreas9 <andkarido@gmail.com>
2023-04-15 22:55:05 +0200

    added text

--- src/routes/medicament/+page.svelte
+++ src/routes/medicament/+page.svelte
@@ -40,7 +40,9 @@
 			vomissements incoercibles et absence de voie veineuse [15].
 		</li>
 	</ul>
+</section>
 
+<section class="medicament">
 	<h2>Des croyances surprenantes</h2>
 
 	<p>
@@ -49,6 +51,154 @@
 		rationnel. Enfin couper les suppositoires en deux est une pratique courante, mais les
 		laboratiores pharmaceutiques ne peuvent garantir la répartition homogène de la substance
 		active, sauf si le suppositoire est sécable.
+	</p>
+</section>
+
+<section class="medicament">
+	<h2>Mon enfant doit prendre un MÉDICAMENT PAR INHALATION</h2>
+
+	<p>
+		Les médicaments sous forme de spray (aérosol) se mélangent à l’air et seront inhalés par
+		votre enfant en quelques respirations.
+	</p>
+</section>
+
+<section class="medicament">
+	<h2>DESCRIPTION DE LA TECHNIQUE D’INHALATION</h2>
+
+	<p>
+		Comment procéder ?  Pour les bébés Le médicament vaporisé par le spray-inhalateur est
+		administré à travers une chambre en plastique munie d’un masque. En respirant par le masque,
+		votre enfant va inhaler le médicament.
+	</p>
+
+	<p>
+		Comment procéder ?  Pour les bébés Le médicament vaporisé par le spray-inhalateur est
+		administré à travers une chambre en plastique munie d’un masque. En respirant par le masque,
+		votre enfant va inhaler le médicament.
+	</p>
+
+	<p>Asseyez votre bébé sur vos genoux, dos contre vous, puis :</p>
+
+	<ul>
+		<li>enlevez le bouchon</li>
+		<li>secouez l’inhalateur</li>
+		<li>
+			introduisez l’embout de l’inhalateur dans l’orifice de la chambre d’inhalation, à
+			l’extrémité opposée du masque
+		</li>
+		<li>appliquez le masque sur le visage de l’enfant de manière étanche</li>
+		<li>administrez une dose de médicament (1 push de spray) en appuyant sur l’inhalateur</li>
+		<li>
+			comptez alors 5 respirations de l’enfant ou 5 secondes (vous entendez le bruit des
+			mouvements de la valve) par dose de spray.
+		</li>
+	</ul>
+
+	<p>
+		Pour les enfants à partir de 4 à 5 ans Le médicament est administré à travers une chambre
+		d’inhalation sans masque :
+	</p>
+
+	<ul>
+		<li>Enlevez le bouchon</li>
+		<li>secouez l’inhalateur</li>
+		<li>
+			introduisez l’extrémité de l’inhalateur dans l’orifice de la chambre d’inhalation, à
+			l’extrémité opposée de l’embout.
+		</li>
+		<li>mettez l’embout de la chambre directement dans la bouche de votre enfant</li>
+		<li>
+			veillez à ce qu’il tienne l’embout dans la bouche de manière étanche en fermant les
+			lèvres autour
+		</li>
+		<li>administrez une dose de médicament (1 push de spray) en appuyant sur l’inhalateur</li>
+		<li>
+			comptez alors 5 respirations de l’enfant ou 5 secondes (vous entendez le bruit des
+			mouvements de la valve) par dose de spray.
+		</li>
+	</ul>
+</section>
+
+<section class="medicament">
+	<h2>Chambres d’inhalation</h2>
+
+	<p>Aérochamber®</p>
+	<ul>
+		<li>0 à 18 mois = orange</li>
+		<li>1 à 5 ans = jaune</li>
+		<li>plus de 5 ans = bleu</li>
+	</ul>
+
+	<p>Comment nettoyer la chambre d’inhalation ?</p>
+
+	<ul>
+		<li>Démontez l’avant et l’arrière de la chambre d’inhalation</li>
+		<li>
+			laissez tremper les pièces 15 minutes dans une solution d’eau tiède et de liquide
+			vaisselle ou de savon
+		</li>
+		<li>laissez sécher à l’air libre en position verticale, sans essuyer</li>
+		<li>remontez la chambre une fois que le tout est bien sec.</li>
+	</ul>
+</section>
+
+<section class="medicament">
+	<h2>MON ENFANT DOIT PRENDRE UN BRONCHODILATATEUR</h2>
+
+	<h3>À QUOI SERT UN BRONCHODILATATEUR ?</h3>
+
+	<p>
+		Ce sont des médicaments qui aident votre enfant à mieux respirer. Ils dilatent ses voies
+		respiratoires et permettent donc un meilleur passage de l’air dans ses poumons. Différents
+		bronchodilatateurs de courte durée d’action existent en France ✓ salbutamol (Ventolin®) ✓
+		terbutaline (Bricanyl®). Ils agissent rapidement en 5 minutes et leur action dure pendant 4
+		heures. Les inhalateurs de bronchodilatateurs de courte durée sont de couleur bleue.
+	</p>
+</section>
+
+<section class="medicament">
+	<h2>LE MÉDICAMENT PEUT-IL AVOIR DES EFFETS INDÉSIRABLES ?</h2>
+
+	<p>
+		Après le traitement bronchodilatateur, votre enfant peut montrer des signes d’agitation
+		accompagnés parfois de tachycardie (battements du cœur plus rapides). Cette tachycardie est
+		transitoire et sans conséquence.
+	</p>
+</section>
+
+<section class="medicament">
+	<h2>MON ENFANT DOIT PRENDRE DES CORTICOSTEROIDES</h2>
+
+	<h3>À QUOI SERVENT LES CORTICOSTÉROÏDES ?</h3>
+
+	<p>
+		Les médicaments corticoïdes sont les médicaments qui soignent l’inflammation lors d’asthme.
+		Ils sont à prendre à dose fixe matin et soir. Différentes sortes de corticoïdes en
+		inhalation existent en France :
+	</p>
+
+	<ul>
+		<li>budénoside (Pulmicort®)</li>
+		<li>fluticasone (Flixotide®): spray-inhalateur de couleur orange.</li>
+	</ul>
+
+	<p>
+		Ils sont parfois associés à des bonchodilatateurs de longue durée d’action. Ces broncho-
+		dilatateurs ont une durée d’action de 12 heures. Ces associations de médicaments sont à
+		prendre en dose fixe matin et soir :
+	</p>
+
+	<ul><li>salmétérol et fluticasone (Seretide®) : inhalateur de couleur violette</li></ul>
+</section>
+
+<section class="medicament">
+	<h2>LE MÉDICAMENT PEUT-IL AVOIR DES EFFETS INDÉSIRABLES ?</h2>
+	<p>
+		Si votre enfant doit prendre des corticoïdes par inhalation, il est important de bien rincer
+		la bouche de votre enfant après le traitement en le faisant boire ou manger. Cela permet
+		d’éviter le risque d’infection par un champignon (le Candida) dans la bouche. Si vous avez
+		utilisé une chambre d’inhalation munie d’un masque, lavez-lui également le visage.
 	</p>
 </section>
 
